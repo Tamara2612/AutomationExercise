cba7dec12a5cdb71848a539a36f9c01e4e3e624e
TAMARA-PC\marin <marinkovictamara26@gmail.com>
2023-04-01 12:37:20 +0200

    Initial Commit

---
+++ AutomationExercise/Helpers/Test Constants.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationExercise.Helpers
+{
+    public static class TestConstants
+    {
+        //existing user data 
+        public const string Username = " marinkovictamara26@gmail.com";
+        public const string Password = "Ukica070721";
+
+        //new user data
+        public const string FirstName = "Tamara";
+        public const string LastName = "Marinkovic";
+        public const string Address = "Bulevar Kralja Petra 9";
+        public const string Country = "Canada";
+        public const string State = "Ontario";
+        public const string City = "Montrael";
+        public const string Zipcode = "10010";
+        public const string Phone = "0800100100";
+    }
+}
---
+++ AutomationExercise/Steps/AuthenticationSteps.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using AutomationExercise.Helpers;
+using AutomationExercise.Pages;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace AutomationExercise.Steps
+{
+    [Binding]
+    public class AuthenticationSteps : Base
+    {
+
+        Utilities ut = new Utilities(Driver);
+        HeaderPage hp = new HeaderPage(Driver);
+
+        [Given(@"User opens sign in page")]
+        public void GivenUserOpensSignInPage()
+        {
+            ut.ClickonElement(hp.loginLink);
+        }
+        
+        [Given(@"user enters correct credentials")]
+        public void GivenUserEntersCorrectCredentials()
+        {
+            AuthenticationPage ap = new AuthenticationPage(Driver);
+            ut.EnterTextinElement(ap.loginEmail, TestConstants.Username);
+            ut.EnterTextinElement(ap.loginPassword, TestConstants.Password);
+        }
+        
+        [When(@"user submits login form")]
+        public void WhenUserSubmitsLoginForm()
+        {
+            AuthenticationPage ap = new AuthenticationPage(Driver);
+            ut.ClickonElement(ap.loginBtn);
+        }
+        
+        [Then(@"user will be logged in")]
+        public void ThenUserWillBeLoggedIn()
+        {
+            Assert.True(ut.ElementisDisplayed(hp.deleteAcc),"User is NOT logged in");
+        }
+
+        [Given(@"enters '(.*)'name and valid email address")]
+        public void GivenEntersNameAndValidEmailAddress(string name)
+        {
+            AuthenticationPage ap = new AuthenticationPage(Driver);
+            ut.EnterTextinElement(ap.name, name);
+            ut.EnterTextinElement(ap.signupEmail, ut.GenerateRandomEmail());
+        }
+
+        [Given(@"user clicks on SignUp button")]
+        public void GivenUserClicksOnSignUpButton()
+        {
+            AuthenticationPage ap = new AuthenticationPage(Driver);
+            ut.ClickonElement(ap.signupBtn);
+        }
+        
+
+        [When(@"user fills in all required fields")]
+        public void WhenUserFillsInAllRequiredFields()
+        {
+            SignupPage sp = new SignupPage(Driver);
+            ut.EnterTextinElement(sp.password,TestConstants.Password);
+            ut.EnterTextinElement(sp.firstName, TestConstants.FirstName);
+            ut.EnterTextinElement(sp.firstName, TestConstants.LastName);
+            ut.EnterTextinElement(sp.address, TestConstants.Address);
+            ut.DropdownSelect(sp.country, TestConstants.Country);
+            ut.EnterTextinElement(sp.state, TestConstants.State);
+            ut.EnterTextinElement(sp.city, TestConstants.City);
+            ut.EnterTextinElement(sp.zipcode, TestConstants.Zipcode);
+            ut.EnterTextinElement(sp.mobile, TestConstants.Phone);
+        }
+
+        [When(@"submits the signup form")]
+        public void WhenSubmitsTheSignupForm()
+        {
+            SignupPage sp = new SignupPage(Driver);
+            Driver.FindElement(sp.form).Submit();
+        }
+
+        [Then(@"user will get '(.*)' success message")]
+        public void ThenUserWillGetSuccessMessage(string message)
+        {
+            AccountCreatedPage acp = new AccountCreatedPage(Driver);
+            Assert.True(ut.TextPresentinElement(message),"User did NOT get expected success message");
+            ut.ClickonElement(acp.continueBtn);
+        }
+
+    }
+}
